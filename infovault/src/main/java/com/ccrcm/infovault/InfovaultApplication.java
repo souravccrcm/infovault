028f6a80201a6df2756f8443d9d65cf4955ce573
Sourav <Sourav.karolly@claimscarercm.com>
2026-02-06 14:56:20 +0530

    intial commit

---
+++ infovault/src/main/java/com/ccrcm/infovault/InfovaultApplication.java
@@ -0,0 +1,13 @@
+package com.ccrcm.infovault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfovaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfovaultApplication.class, args);
+	}
+
+}
---
+++ infovault/src/test/java/com/ccrcm/infovault/InfovaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccrcm.infovault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfovaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
