d5543b5cb2772c4bf8ba6430904b72b985b333d3
Hrithik Prem <Hrithik.prem@claimscarercm.com>
2026-02-09 10:59:51 +0530

    excepyion handler package update

--- infovault/src/main/java/com/ccrcm/infovault/globalExceptionHandler/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ccrcm.infovault.globalExceptionHandler;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
