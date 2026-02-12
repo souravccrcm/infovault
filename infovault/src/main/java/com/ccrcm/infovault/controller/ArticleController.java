3dacf68fe1a6893dbc8e2ebfdc2941b565beac93
Hrithik Prem <Hrithik.prem@claimscarercm.com>
2026-02-12 19:23:34 +0530

    Add IOException handling to save method in ArticleController

--- infovault/src/main/java/com/ccrcm/infovault/controller/ArticleController.java
+++ infovault/src/main/java/com/ccrcm/infovault/controller/ArticleController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -21,7 +22,7 @@ public class ArticleController {
     public ArticleResponse save(
             @Valid @ModelAttribute ArticleRequest request,
             @RequestParam(required = false) MultipartFile file
-    ) {
+    ) throws IOException {
         return articleService.save(request, file);
     }
 
