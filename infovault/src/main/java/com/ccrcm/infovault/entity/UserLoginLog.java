905bcd802bbabae7cf59d89ca473d470efe234f3
Sourav <Sourav.karolly@claimscarercm.com>
2026-02-17 11:54:15 +0530

    home dashboard count api correction

--- infovault/src/main/java/com/ccrcm/infovault/entity/UserLoginLog.java
+++ infovault/src/main/java/com/ccrcm/infovault/entity/UserLoginLog.java
@@ -15,16 +15,22 @@ public class UserLoginLog {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "user_id")
     private Long userId;
 
+    @Column(name = "login_time")
     private LocalDateTime loginTime;
 
+    @Column(name = "logout_time")
     private LocalDateTime logoutTime;
 
+    @Column(name = "ip_address")
     private String ipAddress;
 
+    @Column(name = "user_agent")
     private String userAgent;
 
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 }
 
