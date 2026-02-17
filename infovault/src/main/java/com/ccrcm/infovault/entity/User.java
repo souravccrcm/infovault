feba7ed112c43636f6d37f5481a57b7716f2232c
Hrithik Prem <Hrithik.prem@claimscarercm.com>
2026-02-17 16:56:03 +0530

    Add first name and last name fields to User entity

--- infovault/src/main/java/com/ccrcm/infovault/entity/User.java
+++ infovault/src/main/java/com/ccrcm/infovault/entity/User.java
@@ -17,7 +17,9 @@ public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
     private String email;
     private Boolean active;
