d7cac086b378a9db8d82e3f8d15b7ca15c98b473
Sourav <Sourav.karolly@claimscarercm.com>
2026-03-10 18:01:48 +0530

    Simplify search relevance ranking to binary title vs non-title scoring

--- infovault/src/main/java/com/ccrcm/infovault/specification/ArticleSpecification.java
+++ infovault/src/main/java/com/ccrcm/infovault/specification/ArticleSpecification.java
@@ -81,14 +81,7 @@ public class ArticleSpecification {
 
                 Expression<Integer> relevanceScore = cb.<Integer>selectCase()
                         .when(cb.like(cb.lower(root.get("title")), pattern), 1)
-                        .when(
-                                cb.and(
-                                        cb.notLike(cb.lower(root.get("title")), pattern),
-                                        cb.like(cb.lower(root.get("articleContent")), pattern)
-                                ),
-                                2
-                        )
-                        .otherwise(3);
+                        .otherwise(2);
 
                 query.orderBy(cb.asc(relevanceScore));
             }
